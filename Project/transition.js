bda7b26011b31fb1438e8760d4fe58fc52665d19
Victor <chichikin27@mail.ru>
2023-09-19 13:11:53 +0300

    Добавлен обработчик событий в бот

--- Project/transition.js
+++ Project/transition.js
@@ -16,3 +16,26 @@ const savedTheme = localStorage.getItem('theme');
 if (savedTheme && savedTheme === 'dark') {
     document.body.classList.add('dark-theme');
 }
+
+function sendMessage() {
+    const userInput = document.getElementById('userInput');
+    const chatMessages = document.getElementById('chatMessages');
+
+
+    if (userInput.value.trim() !== '') {
+        chatMessages.innerHTML += `<div class="user-message">${userInput.value}</div>`;
+        userInput.value = '';
+
+
+        setTimeout(() => {
+            chatMessages.innerHTML += '<div class="bot-message">Привет, я бот. Как я могу помочь?</div>';
+        }, 1000);  // задержка в 1 секунду
+    }
+}
+
+document.getElementById('sendButton').addEventListener('click', sendMessage);
+userInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
